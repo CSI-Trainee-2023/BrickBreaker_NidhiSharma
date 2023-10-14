6eb27d43c659c9660082af628bef499c6789c024
Nidhisharma2910 <nidhi2112sharma@gmail.com>
2023-10-14 21:10:18 +0530

    function for bricks, ball, score

--- brick.js
+++ brick.js
@@ -41,3 +41,84 @@ for (c =0; c<brickColumnCount; c++){
 document.addEventListener('keydown', keyDownHandler, false);
 document.addEventListener('keyup', keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
+
+//Anchor paddle movement to mouse movement
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
+
+function keyDownHandler(e){
+    if(e.keyCode === 39){
+        rightPressed = true;
+    }
+    else if (e.keyCode === 37){
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e){
+    if(e.keyCode === 39){
+        rightPressed = false;
+    }
+    else if (e.keyCode === 37){
+        leftPressed = false;
+    }
+}
+
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x,y,ballRadius,0,Math.PI*2); 
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight); 
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks(){
+    for(c=0; c < brickColumnCount; c++){
+        for(r=0; r < brickRowCount; r++){
+            if(bricks[c][r].status === 1){
+                let brickX = (c* (brickWidth + brickPadding)) + brickOffsetLeft;
+                let brickY = (r* (brickHeight+brickPadding)) + brickOffsetTop;
+                bricks[c][r].x=brickX;
+                bricks[c][r].y=brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                brickcolor(r);
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function brickcolor(x){
+    if(x===0){
+        ctx.fillStyle = '#6600cc';
+    }
+    if(x===1){
+        ctx.fillStyle = 'green';
+    }
+    if(x===2){
+        ctx.fillStyle = 'pink';
+    }
+    if(x===3){
+        ctx.fillStyle = 'orange';
+    }
+    
+}
+
+function drawScore(){
+    ctx.font = '18px Arial';
+    ctx.fillStyle = 'brown';
+    ctx.fillText('score: '+ score, 8, 20); 
+}
